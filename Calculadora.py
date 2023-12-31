8d9d44b1a8e451902276f559c9e25a2ec06173f1
kikeando <enriquegcp@gmail.com>
2023-12-31 02:28:27 -0800

    add Calculadora.py

---
+++ Calculadora.py
@@ -0,0 +1,27 @@
+#-Calculadora-#
+#-Posible suma-#
+print (" A continuación tendra una calculadora")
+def math_operation():
+  while 1 == 1
+  print ("Choose the math operation you wanna do")
+  print ("1- addition")
+  print ("2- substration")
+  print ("3- multiplication")
+  print ("4- divisition")
+  try: 
+    opertation = int(input())
+  except ValueError:
+    valor = 0
+    if 
+class suma():
+    try:
+      n1 = int(input("put any number"))
+      n2 = int(input("Put another number again"))
+      answer = n1 + n2
+      print (answer)
+    except ValueError:
+     print ("Plese insert only a number")
+    except Exception as a:
+     print (" UNKNOW ERROR", a)
+
+print (suma())
